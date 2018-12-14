09e07e26fca390603df7faa4015e366aab75c685
agungdh <agungdh@live.com>
2018-12-14 09:00:02 +0700

    home

--- application/views/home.php
+++ application/views/home.php
@@ -4,6 +4,27 @@
 	<title>Test Chat</title>
 </head>
 <body>
-	
+	<div id="link">
+		<a href="<?php echo base_url('welcome/logout'); ?>">Logout</a>
+		<br>
+		<p>Username: <?php echo $this->session->user->username; ?></p>
+		<p>Nama: <?php echo $this->session->user->name; ?></p>
+	</div>
+
+	<div id="form">
+		<label>To Username</label>
+		<input type="text" name="username">
+		<br>
+
+		<label>Messages</label>
+		<textarea name="messages" rows="10" cols="50"></textarea>
+		<br>
+	</div>
+
+	<hr>
+
+	<div id="chat">
+		
+	</div>
 </body>
 </html>
