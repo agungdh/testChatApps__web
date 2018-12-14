d21b0fd5e844b03a673485a6b55b6eab91a3fdf1
agungdh <agungdh@live.com>
2018-12-14 09:58:48 +0700

    sejadinya juga :(

--- application/views/home.php
+++ application/views/home.php
@@ -13,12 +13,14 @@
 
 	<div id="form">
 		<label>To Username</label>
-		<input type="text" name="username">
+		<input type="text" id="username">
 		<br>
 
-		<label>Messages</label>
-		<textarea name="messages" rows="10" cols="50"></textarea>
+		<label>Message</label>
+		<textarea id="message" rows="10" cols="50"></textarea>
 		<br>
+
+		<button onclick="send()">Send</button>
 	</div>
 
 	<hr>
@@ -32,5 +34,34 @@
 	<script type="text/javascript" src="<?php echo base_url('assets/socket.io.dev.js'); ?>"></script>
 	<!-- Socket IO -->
 	<!-- <script type="text/javascript" src="<?php echo base_url('assets/socket.io.js'); ?>"></script> -->
+
+	<script type="text/javascript">
+		var userID = <?php echo $this->session->user->id; ?>;
+		var socket = io('localhost:3000');
+	</script>
+
+	<script type="text/javascript">
+		function clear() {
+			$("#message").val('');
+		}
+
+		function send() {
+			data = {
+				userID: userID,
+				username: $("#username").val(),
+				message: $("#message").val(),
+			};
+
+			socket.emit('sendChat', data);
+			clear();
+			return false;
+		}
+		
+	    socket.on('receiveChat__' + userID, function(msg){
+	      $('#chat').append(
+	      	'<p>' + msg + '</p><br>'
+	      	);
+	    });	
+	</script>
 </body>
 </html>
